0b6d2bc139f05bb126f90c3b77f027693b09bc27
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-02 22:50:17 +0300

    added cinema service with zod validation and type safe

---
+++ admin-client/src/utils/CinemaValidationsSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { CinemaValidation } from "./constants/cinemaConstants";
+
+export const cinemaSchema = z.object({
+    id: z.string().optional(),
+    city: z.string().min(3, CinemaValidation.city).max(150, CinemaValidation.city),
+    name: z.string().min(4, CinemaValidation.name).max(100, CinemaValidation.name),
+    halls: z.array(z.string()),
+    snacks: z.array(z.object({
+        name: z.string().min(1, CinemaValidation.snackName).max(100, CinemaValidation.snackName),
+        description: z.string().min(1, CinemaValidation.snackDescription).max(300, CinemaValidation.snackDescription),
+        price: z.number().min(0.10, CinemaValidation.snackPrice).max(1000, CinemaValidation.snackPrice)
+    }))
+})
+
+export type Cinema = z.infer<typeof cinemaSchema>;
---
+++ admin-client/src/utils/constants/cinemaConstants.ts
@@ -0,0 +1,7 @@
+export const CinemaValidation = {
+    city: "City must be between 3 and 150 characters long.",
+    name: "Cinema must be between 4 and 100 characters long.",
+    snackName: "Name must be between 2 and 100 character long.",
+    snackDescription: "Description must be between 1 and 300 characters long.",
+    snackPrice: "Price must be between 0.10 and 1000"
+}
