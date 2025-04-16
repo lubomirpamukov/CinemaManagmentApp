01e4eda7d2ca8ed25dd9f9fa5217b1188b3e4d20
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-16 18:47:49 +0300

    add UserPage component

---
+++ admin-client/src/pages/UserPage.tsx
@@ -0,0 +1,49 @@
+import { z } from "zod";
+
+import { usePaginated } from "../hooks";
+import Pagination from "../components/buttons/Pagination";
+import { userSchema } from "../utils";
+import Spinner from "../components/Spinner";
+import styles from "./UserPage.module.css";
+import UserList from "../components/user/UserList";
+
+const UserPage: React.FC = () => {
+  const {
+    data: users,
+    currentPage,
+    totalPages,
+    setCurrentPage,
+    loading,
+    error,
+  } = usePaginated("/users", 4, z.array(userSchema));
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <div className={styles.error}>{error}</div>;
+  }
+
+  if (!users || users.length <= 0) {
+    return <div className={styles.error}>No users found</div>;
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Users</h1>
+      </header>
+      <main>
+        <UserList users={users} />
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </main>
+    </>
+  );
+};
+
+export default UserPage;
