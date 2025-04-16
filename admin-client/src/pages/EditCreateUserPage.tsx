82b097f34a5df2767da278c053f9d6028d0e025e
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-16 18:46:56 +0300

    add EditCreateUser component

---
+++ admin-client/src/pages/EditCreateUserPage.tsx
@@ -0,0 +1,24 @@
+import { useParams } from "react-router-dom";
+
+import { useUserById } from "../hooks";
+import Spinner from "../components/Spinner";
+import styles from "./EditCreateUserPage.module.css";
+import UserForm from "../components/user/UserForm";
+
+const EditCreateUserPage = () => {
+  const { userId } = useParams<{ userId: string }>();
+  const isEditMode = Boolean(userId);
+  const { user, loading, error } = useUserById(userId);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+  
+  return <UserForm isEditMode={isEditMode} user={user} />;
+};
+
+export default EditCreateUserPage;
