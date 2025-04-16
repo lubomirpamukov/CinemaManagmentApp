d8ae1d8d4b1f5e607ee549b5a07e16ed2356593a
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-16 18:47:23 +0300

    refactor movie page to use pagination

--- admin-client/src/pages/MoviePage.tsx
+++ admin-client/src/pages/MoviePage.tsx
@@ -1,17 +1,49 @@
-import React,{ Suspense} from "react";
+import { z } from "zod";
+
+import { usePaginated } from "../hooks";
+import Pagination from "../components/buttons/Pagination";
+import Spinner from "../components/Spinner";
+import { movieSchema } from "../utils";
 import MovieList from "../components/movie/MovieList";
 import styles from "./MoviePage.module.css";
 
 const MoviePage: React.FC = () =>{
+    const {
+        data: movies,
+        currentPage,
+        totalPages,
+        setCurrentPage,
+        loading,
+        error,
+    } 
+    = usePaginated("/movies", 3, z.array(movieSchema))
+
+    if(loading){
+        return <Spinner />
+    }
+
+    if(error){
+        return <div>{error}</div>
+    }
+
+    if(!movies || movies.length <= 0){
+        return <div>No movies found</div>
+    }
+
+
+    
     return (
         <section className={styles.moviePage}>
             <header className={styles.moviePageHeader}>
                 <h1>Movies</h1>
             </header>
             <main className={styles.moviePageContent}>
-            <Suspense fallback={<p>Loading cinemas...</p>}>
-                <MovieList />
-            </Suspense>
+                <MovieList movies={movies} />
+                <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={setCurrentPage}
+                />
             </main>
         </section>
     )
