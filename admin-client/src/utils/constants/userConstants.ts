869aedd2d7ed37b59fc2afb1568d10f74f7f35ec
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-16 18:48:39 +0300

    added user validation

---
+++ admin-client/src/utils/constants/userConstants.ts
@@ -0,0 +1,30 @@
+import { User } from "../UserValidationSchema";
+
+export const DEFAULT_USER_VALUES: User = {
+    id: "",
+    userName: "",
+    name: "",
+    email: "",
+    password: "",
+    contact: "",
+    address: {
+      line1: "",
+      city: "",
+      state: "",
+      zipcode: ""
+    },
+    geoLocation: {
+      lat: 0,
+      long: 0
+    }
+  };
+
+export const UserValidation = {
+    name: "Name must be between 4 and 100 characters long.",
+    email: "Email must be a valid email address and with maximum 100 chacaters.",
+    password: "Password must be between 8 and 100 characters long.",
+    contact: "Contact number must be a valid phone number.",
+    addressLine1: "Address must be between 4 and 100 characters long.",
+    city: "City must be between 2 and 100 characters long.",
+    zipcode: "Zipcode must be between 4 and 10 characters long.",
+}
