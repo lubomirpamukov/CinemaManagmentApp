9fef368859350d5e4871a0fb8c5cf0ad1a6dd4f2
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-16 18:32:24 +0300

    added error baundry + client feature

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -1,28 +1,50 @@
-import { BrowserRouter as Router ,Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+
 import MoviePage from "./pages/MoviePage";
 import CreateMoviePage from "./pages/CreateMoviePage";
 import Navigation from "./components/Navigation/Navigation";
-import CinemaPage from "./pages/CinemaPage"
+import CinemaPage from "./pages/CinemaPage";
 import SnackPage from "./pages/SnackPage";
 import CreateHallPage from "./pages/CreateHallPage";
 import EditCinemaPage from "./pages/EditCinemaPage";
 import CreateCinemaPage from "./pages/CreateCinemaPage";
+import UserPage from "./pages/UserPage";
+import EditCreateUserPage from "./pages/EditCreateUserPage";
+import ErrorFallback from "./components/ErrorFallback";
 
 function App() {
-  console.log("App component rendered");
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/cinemas/:cinemaId/snacks/edit" element={<SnackPage />} />
-        <Route path="/cinemas/:cinemaId/hall/create" element={<CreateHallPage />} />
-        <Route path="/cinemas/:cinemaId/edit" element={<EditCinemaPage />}/>
-        <Route path="/cinemas" element={<CinemaPage />}/>
-        <Route path="/cinemas/create" element={<CreateCinemaPage />}/>
-        <Route path="/movies" element={<MoviePage />} />
-        <Route path="/movies/create" element={<CreateMoviePage />} />
-      </Routes>
-    </Router>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onError={(error, info) => {
+        // Optional: Log error to monitoring service
+        console.error("Application error:", error);
+        console.error("Component stack:", info.componentStack);
+      }}
+    >
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/users" element={<UserPage />} />
+          <Route path="/users/edit" element={<EditCreateUserPage />} />
+          <Route path="/users/:userId/edit" element={<EditCreateUserPage />} />
+          <Route
+            path="/cinemas/:cinemaId/snacks/edit"
+            element={<SnackPage />}
+          />
+          <Route
+            path="/cinemas/:cinemaId/hall/create"
+            element={<CreateHallPage />}
+          />
+          <Route path="/cinemas/:cinemaId/edit" element={<EditCinemaPage />} />
+          <Route path="/cinemas" element={<CinemaPage />} />
+          <Route path="/cinemas/create" element={<CreateCinemaPage />} />
+          <Route path="/movies" element={<MoviePage />} />
+          <Route path="/movies/create" element={<CreateMoviePage />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
