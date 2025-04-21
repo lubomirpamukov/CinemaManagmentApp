4c543c4f410588f9e1c8cba95230f382562e806f
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-21 14:04:17 +0300

    fixed pagination to work correctly without fetch the whole data

--- admin-client/src/hooks/usePaginated.ts
+++ admin-client/src/hooks/usePaginated.ts
@@ -3,29 +3,27 @@ import { ZodSchema } from "zod";
 
 export const usePaginated = <T>(
   endpoint: string,
-  limit: number,
+  pageSize: number,
   schema: ZodSchema<T[]>
 ) => {
-  const [allData, setAllData] = useState<T[]>([]);
   const [data, setData] = useState<T[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  // Fetch all data once refactor when backend is ready !!!
   useEffect(() => {
     const fetchAllData = async () => {
       setLoading(true);
       try {
-        const response = await fetch(`http://localhost:3000${endpoint}`);
+        const response = await fetch(`http://localhost:3000${endpoint}?_page=${currentPage}&_per_page=${pageSize}`);
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
         const result = await response.json();
-        const parsedData = schema.parse(result);
-        setAllData(parsedData);
-        setTotalPages(Math.ceil(parsedData.length / limit));
+        const parsedData = schema.parse(result.data);
+        setData(parsedData);
+        setTotalPages(result.pages);
         setError(null);
       } catch (err) {
         setError("Error fetching data");
@@ -35,14 +33,7 @@ export const usePaginated = <T>(
     };
 
     fetchAllData();
-  }, [endpoint, limit]);
-
-  // Update paginated data when currentPage or allData changes
-  useEffect(() => {
-    const startIndex = (currentPage - 1) * limit;
-    const endIndex = startIndex + limit;
-    setData(allData.slice(startIndex, endIndex));
-  }, [allData, currentPage, limit]);
+  }, [endpoint, currentPage]);
 
   return { data, currentPage, totalPages, loading, error, setCurrentPage };
 };
