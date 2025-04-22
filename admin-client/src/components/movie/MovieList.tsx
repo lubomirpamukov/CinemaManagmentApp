b581b4251bd7e8eccd678fe47db4fc3e602327cf
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-22 11:32:57 +0300

    added searchbar to movies

--- admin-client/src/components/movie/MovieList.tsx
+++ admin-client/src/components/movie/MovieList.tsx
@@ -3,23 +3,25 @@ import React from "react";
 import MovieCard from "./MovieCard";
 import styles from "./MovieList.module.css";
 import { Movie } from "../../utils";
+import SearchBar from "../SearchBar";
 
 type MovieListProps = {
-  movies: Movie[]
-}
+  movies: Movie[];
+};
 
-const MovieList: React.FC<MovieListProps> = ({movies}) => {
-
-
+const MovieList: React.FC<MovieListProps> = ({ movies }) => {
   return (
-    <div className={styles.movieList}>
-      {movies.map((movie) => (
-        <MovieCard 
-        key={movie.id} 
-        movie={movie} 
-        />
-      ))}
-    </div>
+    <>
+      <SearchBar
+        onSearch={(query) => console.log(query)}
+        placeholder="Search Movies"
+      />
+      <div className={styles.movieList}>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </>
   );
 };
 
