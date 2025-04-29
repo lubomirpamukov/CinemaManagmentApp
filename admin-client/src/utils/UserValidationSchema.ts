166cbd3291327a7d823be7a1ea21045ba61da6f1
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-29 13:53:14 +0300

    removed user properties

--- admin-client/src/utils/UserValidationSchema.ts
+++ admin-client/src/utils/UserValidationSchema.ts
@@ -2,18 +2,6 @@ import { z } from "zod";
 
 import { UserValidation } from "./constants/userConstants";
 
-export const userAddressSchema = z.object({
-    line1: z.string().min(4,UserValidation.addressLine1).max(100, UserValidation.addressLine1).optional(),
-    city: z.string().min(2,UserValidation.city).max(100, UserValidation.city).optional(),
-    state: z.string().min(2,UserValidation.city).max(100, UserValidation.city).optional(),
-    zipcode: z.string().min(4,UserValidation.zipcode).max(10, UserValidation.zipcode).optional(),
-})
-
-export const userGeoLocationSchema = z.object({
-    lat: z.number().min(-90).max(90).optional(),
-    long: z.number().min(-180).max(180).optional()
-  });
-
 export const userSchema = z.object({
     id: z.string(),
     userName: z.string().min(4, UserValidation.name).max(100, UserValidation.name),
@@ -21,8 +9,6 @@ export const userSchema = z.object({
     email: z.string().email(UserValidation.email).max(100, UserValidation.email),
     password: z.string().min(8, UserValidation.password).max(100, UserValidation.password),
     contact: z.string().max(15,UserValidation.contact).optional(),
-    address: userAddressSchema.optional(),
-    geoLocation: userGeoLocationSchema.optional(),
 })
 
 export type User = z.infer<typeof userSchema>;
--- admin-client/src/utils/constants/userConstants.ts
+++ admin-client/src/utils/constants/userConstants.ts
@@ -14,7 +14,5 @@ export const UserValidation = {
     email: "Email must be a valid email address and with maximum 100 chacaters.",
     password: "Password must be between 8 and 100 characters long.",
     contact: "Contact number must be a valid phone number.",
-    addressLine1: "Address must be between 4 and 100 characters long.",
-    city: "City must be between 2 and 100 characters long.",
-    zipcode: "Zipcode must be between 4 and 10 characters long.",
 }
+
