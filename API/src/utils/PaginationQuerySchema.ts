e37c74f720bde9213eb274c74c620ea639a3cb66
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-30 17:58:07 +0300

    added pagination

---
+++ API/src/utils/PaginationQuerySchema.ts
@@ -0,0 +1,19 @@
+import z from "zod";
+
+export const getPaginationQuerySchema = z.object({
+    page: z
+        .string()
+        .optional()
+        .transform((val) => parseInt(val || '1'))
+        .refine((val) => val > 0, {
+            message: 'Page must be a positive number'
+        }),
+    limit: z
+        .string()
+        .optional()
+        .transform((val) => parseInt(val || '10'))
+        .refine((val) => val > 0, {
+            message: 'Limit must be a positive number'
+        }),
+    search: z.string().optional()
+});
---
+++ API/src/utils/PaginationUtils.ts
@@ -0,0 +1,38 @@
+import { Model, Document } from "mongoose";
+
+interface PaginationResult<T> {
+    data: T[];
+    totalItems: number;
+    totalPages: number;
+    currentPage: number;
+}
+
+interface PaginationOptions {
+    page: number;
+    limit: number;
+    searchQuery?: object;
+    selectFields?: string;
+}
+
+export const paginate = async <T extends Document>(
+    model: Model<T>,
+    options: PaginationOptions
+): Promise<PaginationResult<T>> => {
+    const { page, limit, searchQuery = {}, selectFields = "" } = options;
+
+    const skip = (page - 1) * limit;
+
+    const [totalItems, data] = await Promise.all([
+        model.countDocuments(searchQuery),
+        model.find(searchQuery).skip(skip).limit(limit).select(selectFields), // Fetch paginated data
+    ]);
+
+    const totalPages = Math.ceil(totalItems / limit);
+
+    return {
+        data,
+        totalItems,
+        totalPages,
+        currentPage: page,
+    };
+};
