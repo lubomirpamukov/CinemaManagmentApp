89dcbd864ccf7f7bef6f46fb33fc7e5fa9b202d5
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-30 17:56:47 +0300

    added comments for easy navigation

--- API/src/middleware/auth.middleware.ts
+++ API/src/middleware/auth.middleware.ts
@@ -12,12 +12,15 @@ export interface JwtRequest extends Request {
     };
 }
 
+
+//Checks for authorization headers
 export const authentication = (req: JwtRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
         return res.status(401).json('No authorization header found');
     }
 
+    //Extracts jwt token and verifies it
     const token = authHeader.split(' ')[1]; // Token structure is 'Bearer <token>'
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
@@ -28,6 +31,7 @@ export const authentication = (req: JwtRequest, res: Response, next: NextFunctio
     }
 };
 
+//checks if user has the needed role to acess from the given roles
 export function authorizeRoles(allowedRoles: UserRole[]) {
     return (req: JwtRequest, res: Response, next: NextFunction) => {
         const user = req.user;
@@ -41,5 +45,5 @@ export function authorizeRoles(allowedRoles: UserRole[]) {
 }
 
 export const generateToken = (_id: mongoose.Types.ObjectId, role: string) => {
-    return jwt.sign({ _id, role }, JWT_SECRET, { expiresIn: '1h' });
+    return jwt.sign({ _id, role }, JWT_SECRET, { expiresIn: '99h' });
 };
