240da02042e2d7383a1a44ddaaf7087fb7877b5e
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-05-01 19:02:04 +0300

    added login page

--- admin-client/src/pages/LoginPage.tsx
+++ admin-client/src/pages/LoginPage.tsx
@@ -1,17 +1,43 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
+import { useAuth } from "../context/AuthContext";
 import LoginShow from "../components/LoginShow";
 import styles from "./LoginPage.module.css";
-
+//to do refactor to use login hook
 const LoginPage: React.FC = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      const response = await fetch("http://localhost:3123/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || "Login failed");
+      }
+
+      login();
+      navigate("/users");
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
       <h1>hello from loginPage</h1>
-      <LoginShow />
+      <LoginShow onLogin={handleLogin} />
     </>
   );
 };
 
 export default LoginPage;
-
