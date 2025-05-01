6271b637e03547eef97af4ba621ae59bdfa2a090
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-05-01 19:03:42 +0300

    added logout functionality

--- API/src/routes/auth.routes.ts
+++ API/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/auth.controller';
+import { registerUser, loginUser, logoutUser, checkAuth } from '../controllers/auth.controller';
+import { authentication } from '../middleware/auth.middleware';
 
 const authRouter: Router = express.Router();
 
@@ -16,5 +17,19 @@ authRouter.post('/register', registerUser);
  * @access  (Public)
  */
 authRouter.post('/login', loginUser);
+
+/**
+ * @route   POST /api/auth/logout
+ * @desc    Logout a user
+ * @access  (Public)
+ */
+authRouter.post('/logout', logoutUser)
+
+/**
+ * @route   GET /api/auth/check-auth
+ * @desc    Check if the user is authenticated
+ * @access  (Protected)
+ */
+authRouter.get('/check-auth', authentication, checkAuth);
 
 export default authRouter;
