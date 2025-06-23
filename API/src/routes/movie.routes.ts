2eaa3ae1bfd1a5bd94da7c402c94098334c55956
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-06-23 12:51:13 +0300

    added new routes

--- API/src/routes/movie.routes.ts
+++ API/src/routes/movie.routes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
 import { authentication, authorizeRoles } from '../middleware/auth.middleware';
-import { getMovies } from '../controllers/movie.controller';
+import { getMovieById, getMovies } from '../controllers/movie.controller';
 
 const moviesRouter: Router = express.Router();
 
 moviesRouter.get('/', authentication, authorizeRoles(['user']), getMovies);
+moviesRouter.get('/:movieId', authentication, authorizeRoles(['user']), getMovieById)
 
 export default moviesRouter;
