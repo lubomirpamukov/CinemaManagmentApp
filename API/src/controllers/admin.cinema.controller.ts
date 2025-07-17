bcfa04da29b47a90a1d4ac9d44da03830673de76
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-07-17 04:36:11 +0300

    refactor cinemaService and cinemaController + JSDocumentation

--- API/src/controllers/admin.cinema.controller.ts
+++ API/src/controllers/admin.cinema.controller.ts
@@ -1,42 +1,79 @@
 import { Request, Response } from 'express';
 import { getCinemasService, getCinemaByIdService, updateCinemaByIdService } from '../services/adminCinemaService';
+import mongoose from 'mongoose';
+import { cinemaSchema } from '../utils';
 
+/**
+ * @route GET /api/admin/cinemas
+ * @desc Get all cinemas
+ * @access Private (Admin)
+ */
 export const getCinemas = async (req: Request, res: Response) => {
     try {
         const cinemas = await getCinemasService();
-        if (!cinemas) return res.status(404).json({ message: 'Cinema not found' });
-        res.status(200).json(cinemas);
+        res.status(200).json({ cinemas });
     } catch (err: any) {
         if (err.name === 'ZodError') {
-            return res.status(400).json({ error: err.errors });
+            return res.status(400).json({ error: 'Data validation failed', details: err.errors });
         }
         res.status(500).json({ error: err.message });
     }
 };
 
+/**
+ * @route GET /api/admin/cinemas
+ * @desc Get cinema by ID
+ * @access Private (Admin)
+ */
 export const getCinemaById = async (req: Request, res: Response) => {
     try {
-        const cinema = await getCinemaByIdService(req.params.id);
-        console.log('cinema', cinema);
-        console.log('req.params.id', req.params.id);
-        if (!cinema) return res.status(404).json({ message: 'Cinema not found' });
-        res.status(200).json(cinema);
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid cinema ID format.' });
+        }
+
+        const cinema = await getCinemaByIdService(id);
+
+        res.status(200).json({ cinema });
     } catch (err: any) {
         if (err.name === 'ZodError') {
-            return res.status(400).json({ error: err.errors });
+            return res.status(400).json({ error: 'Cinema data validation failed.', details: err.errors });
         }
+
+        if (err.message.includes('not found')) {
+            return res.status(404).json({ error: err.message });
+        }
+
         res.status(500).json({ error: err.message });
     }
 };
 
+/**
+ * @route PATCH /api/admin/cinemas/:id
+ * @desc Updates the data of a cinema object
+ * @access Private (Admin)
+ */
 export const updateCinema = async (req: Request, res: Response) => {
     try {
-        const updatedCinema = await updateCinemaByIdService(req.params.id, req.body);
-        if (!updatedCinema) return res.status(404).json({ message: 'Cinema not found' });
-        res.status(201).json(updatedCinema);
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid cinema ID format.' });
+        }
+
+        const validUpdates = cinemaSchema.partial().parse(req.body);
+
+        const updatedCinema = await updateCinemaByIdService(id, validUpdates);
+
+        res.status(200).json({ cinema: updatedCinema });
     } catch (err: any) {
         if (err.name === 'ZodError') {
-            return res.status(400).json({ error: err.errors });
+            return res.status(400).json({ error: 'Updates validation failed', details: err.errors });
+        }
+
+        if (err.message.includes('not found')) {
+            return res.status(404).json({ error: err.message });
         }
         res.status(500).json({ error: err.message });
     }
--- API/src/services/adminCinemaService.ts
+++ API/src/services/adminCinemaService.ts
@@ -1,49 +1,25 @@
-import { CinemaZod, cinemaSchema } from '../utils/CinemaValidation';
+import { TCinema, cinemaSchema } from '../utils/CinemaValidation';
 import Cinema, { ICinema } from '../models/cinema.model';
+import mongoose from 'mongoose';
 
-export const getCinemasService = async (): Promise<CinemaZod[]> => {
+/**
+ * Fetches all cinemas from the database, transforms them into DTO,
+ * and validates them against the cinema schema.
+ * @throws {ZodError} If the data from the database fails validation.
+ * @returns {Promise<TCinema[]>} Resolves to an array of valid cinema objects.
+ */
+export const getCinemasService = async (): Promise<TCinema[]> => {
     const cinemasFromDB: ICinema[] = await Cinema.find().lean();
 
-    try {
-        const transformedCinemasDTO: CinemaZod[] = cinemasFromDB.map((cinema) => {
-            const cinemaDTO = {
-                id: cinema._id?.toString(),
-                city: cinema.city,
-                name: cinema.name,
-                halls: Array.isArray(cinema.halls) ? cinema.halls.map((hall) => (hall = hall.toString())) : [],
-                imgURL: cinema.imgURL,
-                snacks: cinema.snacks
-                    ? cinema.snacks.map((snack) => ({
-                          id: snack._id?.toString(),
-                          name: snack.name,
-                          description: snack.description,
-                          price: snack.price
-                      }))
-                    : []
-            };
-            return cinemaDTO as CinemaZod;
-        });
-
-        const validatedCinemas: CinemaZod[] = cinemaSchema.array().parse(transformedCinemasDTO);
-        return validatedCinemas;
-    } catch (err: any) {
-        throw new Error(err.message);
-    }
-};
-
-export const getCinemaByIdService = async (id: string): Promise<CinemaZod | null> => {
-    const cinemaFromDB: ICinema | null = await Cinema.findById(id);
-    if (!cinemaFromDB) return null;
-
-    try {
-        const transformedCinemaDTO: CinemaZod = {
-            id: cinemaFromDB._id?.toString(),
-            city: cinemaFromDB.city,
-            name: cinemaFromDB.name,
-            halls: cinemaFromDB.halls || [],
-            imgURL: cinemaFromDB.imgURL,
-            snacks: cinemaFromDB.snacks
-                ? cinemaFromDB.snacks.map((snack) => ({
+    const transformedCinemasDTO: TCinema[] = cinemasFromDB.map((cinema) => {
+        const cinemaDTO = {
+            id: cinema._id?.toString(),
+            city: cinema.city,
+            name: cinema.name,
+            halls: Array.isArray(cinema.halls) ? cinema.halls.map((hall) => hall.toString()) : [],
+            imgURL: cinema.imgURL,
+            snacks: cinema.snacks
+                ? cinema.snacks.map((snack) => ({
                       id: snack._id?.toString(),
                       name: snack.name,
                       description: snack.description,
@@ -51,18 +27,53 @@ export const getCinemaByIdService = async (id: string): Promise<CinemaZod | null
                   }))
                 : []
         };
-        const validatedCinema: CinemaZod = cinemaSchema.parse(transformedCinemaDTO);
-        return validatedCinema;
-    } catch (err: any) {
-        throw new Error(err.message);
-    }
+        return cinemaDTO as TCinema;
+    });
+
+    const validatedCinemas: TCinema[] = cinemaSchema.array().parse(transformedCinemasDTO);
+    return validatedCinemas;
 };
 
-export const updateCinemaByIdService = async (id: string, updates: CinemaZod): Promise<CinemaZod | null> => {
-    if (!id) {
-        throw new Error('Cinema ID is required.');
-    }
+/**
+ * Fetches a specific cinema based on ID from the database, transforms it into DTO,
+ * and validates them against cinema schema.
+ * @param {string} id The ID of the cinema.
+ * @throws {ZodError} If the data from the database fails validation.
+ * @throws {Error} If cinema document dosnt exist throws `Cinema not found`
+ * @returns {Promise<TCinema>} Resolves to a valid cinema object or null if not found.
+ */
+export const getCinemaByIdService = async (id: string): Promise<TCinema> => {
+    const cinemaFromDB: ICinema | null = await Cinema.findById(id).lean();
+    if (!cinemaFromDB) throw new Error('Cinema not found')
 
+    const transformedCinemaDTO: TCinema = {
+        id: cinemaFromDB._id?.toString(),
+        city: cinemaFromDB.city,
+        name: cinemaFromDB.name,
+        halls: Array.isArray(cinemaFromDB.halls) ? cinemaFromDB.halls.map((hall) => hall.toString()) : [],
+        imgURL: cinemaFromDB.imgURL,
+        snacks: Array.isArray(cinemaFromDB.snacks)
+            ? cinemaFromDB.snacks.map((snack) => ({
+                  id: snack._id?.toString(),
+                  name: snack.name,
+                  description: snack.description,
+                  price: snack.price
+              }))
+            : []
+    };
+    const validatedCinema: TCinema = cinemaSchema.parse(transformedCinemaDTO);
+    return validatedCinema;
+};
+
+/**
+ * Updates a cinema's details by its ID.
+ * Assumes the `updates` object has been validated by the controller.
+ * @param {string} id The Id of the cinema to update.
+ * @param {Partial<TCinema>} updates An object containing the validated cinema data to update.
+ * @returns {Promise<Tcinema>} Resloves to the update cinema DTO.
+ * @throws {Error} If the cinema with the given ID is not found.
+ */
+export const updateCinemaByIdService = async (id: string, updates: Partial<TCinema>): Promise<TCinema> => {
     const updatedCinema = await Cinema.findByIdAndUpdate(id, updates, {
         new: true,
         runValidators: true
@@ -72,13 +83,13 @@ export const updateCinemaByIdService = async (id: string, updates: CinemaZod): P
         throw new Error('Cinema not found');
     }
 
-    const cinemaExportDto: CinemaZod = {
+    const cinemaExportDto: TCinema = {
         id: updatedCinema._id?.toString(),
         city: updatedCinema.city,
         name: updatedCinema.name,
-        halls: updatedCinema.halls || [],
+        halls: Array.isArray(updatedCinema.halls) ? updatedCinema.halls.map((h) => h.toString()) : [],
         imgURL: updatedCinema.imgURL,
-        snacks: updatedCinema.snacks
+        snacks: Array.isArray(updatedCinema.snacks)
             ? updatedCinema.snacks.map((snack) => ({
                   id: snack._id?.toString(),
                   name: snack.name,
@@ -88,4 +99,23 @@ export const updateCinemaByIdService = async (id: string, updates: CinemaZod): P
             : []
     };
     return cinemaExportDto;
+};
+
+/**
+ * Adds a hall's ID to a cinemas list of halls.
+ * @param {string} cinemaId The ID of the cinema to update.
+ * @param {mongoose.Types.ObjectId} hallId The ID of the new hall to add.
+ * @param {mongoose.ClientSession} [session] Optional Mongoose session for transactions.
+ */
+export const addHallToCinemaService = async (cinemaId: string, hallId: mongoose.Types.ObjectId, session?: mongoose.ClientSession) => {
+    await Cinema.findByIdAndUpdate(cinemaId, { $push: { halls: hallId } }, { session });
+};
+
+/**
+ * Removes a hall's ID from its parent cinema's array.
+ * @param {string} hallId The ID of the hall to remove.
+ * @param {mongoose.ClientSession} [session] The mongoose session for the transaction.
+ */
+export const removeHallFromCinemaService = async (hallId: string, session?: mongoose.ClientSession): Promise<void> => {
+    await Cinema.updateOne({ halls: hallId }, { $pull: { halls: hallId } }, { session });
 };
