a5bc0853767336032c77f694606907a6ec4919f0
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-07-17 04:50:53 +0300

    refactor admin movie service and controller added JSDocumentation

--- API/src/controllers/admin.movie.controller.ts
+++ API/src/controllers/admin.movie.controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from 'express';
 import { getMoviesService, deleteMovieService, updateMovieService, createMovieService } from '../services/adminMovieService';
+import mongoose from 'mongoose';
+import { movieSchema } from '../utils';
 
+/**
+ * @route GET /api/admin/movies
+ * @desc Get all movies
+ * @access Private (Admin)
+ */
 export const getMovies = async (req: Request, res: Response) => {
     try {
-        const result = await getMoviesService(req.query);
-        if (!result) return res.status(404).json({ message: 'Movie not found' });
-        res.status(200).json(result);
+        const paginatedMovies = await getMoviesService(req.query);
+        res.status(200).json(paginatedMovies);
     } catch (err: any) {
         if (err.name === 'ZodError') {
             return res.status(400).json({ error: err.errors });
@@ -14,38 +20,74 @@ export const getMovies = async (req: Request, res: Response) => {
     }
 };
 
+/**
+ * @route PATCH /api/admin/movies/:id
+ * @desc Updates a Movie's details
+ * @access Private (Admin)
+ */
 export const updateMovie = async (req: Request, res: Response) => {
     try {
-        const updatedMovie = await updateMovieService(req.params.id, req.body);
-        if (!updatedMovie) return res.status(404).json({ message: 'Movie not found' });
-        res.status(201).json(updatedMovie);
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid movie ID format.' });
+        }
+
+        const validatedUpdates = movieSchema.partial().parse(req.body);
+        const updatedMovie = await updateMovieService(id, validatedUpdates);
+        res.status(201).json({ movie: updatedMovie });
     } catch (err: any) {
         if (err.name === 'ZodError') {
-            return res.status(400).json({ error: err.errors });
+            return res.status(400).json({ error: `Invalid update data`, details: err.errors });
+        }
+
+        if (err.message === 'Movie not found') {
+            return res.status(400).json({ error: err.message });
         }
         res.status(500).json({ error: err.message });
     }
 };
 
+/**
+ *@route POST/ api/admin/movies
+ *@desc Create new Movie.
+ *@access Private (Admin)
+ */
 export const createMovie = async (req: Request, res: Response) => {
     try {
-        const newMovie = await createMovieService(req.body);
-        if (!newMovie) return res.status(400).json({ message: 'Movie not created' });
+        const validMovieData = movieSchema.parse(req.body);
+        const newMovie = await createMovieService(validMovieData);
         res.status(201).json(newMovie);
     } catch (err: any) {
         if (err.name === 'ZodError') {
             return res.status(400).json({ error: err.errors });
         }
+
+        //Handle mongoDb unique constraint errors
+        if (err.code === 11000) {
+            return res.status(409).json({ error: 'A movie with the provided title already exists.' });
+        }
         res.status(500).json({ error: err.message });
     }
 };
 
+/**
+ * @route DELETE /api/admin/movies/:id
+ * @desc Deletes a movie by its ID
+ * @access Private (Admin)
+ */
 export const deleteMovie = async (req: Request, res: Response) => {
     try {
-        const deletedMovie = await deleteMovieService(req.params.id);
-        if (!deletedMovie) return res.status(404).json({ message: 'Movie not found' });
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'Invalid movie ID format.' });
+        }
+        await deleteMovieService(id);
         res.status(204).send();
     } catch (err: any) {
+        if (err.message.includes('Movie not found')) {
+            return res.status(404).json({ error: err.message });
+        }
         res.status(500).json({ error: err.message });
     }
 };
--- API/src/services/adminMovieService.ts
+++ API/src/services/adminMovieService.ts
@@ -1,11 +1,20 @@
-import { MovieZod, movieSchema } from '../utils/MovieValidation';
+import { TMovie, TMoviePaginated, movieSchema } from '../utils';
 import { paginate } from '../utils';
-import Movie from '../models/movie.model';
+import Movie, { IMovie } from '../models/movie.model';
 import Session from '../models/session.model';
-import { getPaginationQuerySchema } from '../utils/PaginationQuerySchema';
-import { moviePaginatedSchema } from '../utils/MovieValidation';
+import { getPaginationQuerySchema } from '../utils';
+import { moviePaginatedSchema } from '../utils';
+import mongoose from 'mongoose';
 
-export const getMoviesService = async (query: any) => {
+/**
+ * Fetches a paginated list of movies, with optional searching across title, director, and genre
+ * @param {string} [query.page='1'] The page number for pagination.
+ * @param {string} [query.limit='10'] The number of items per page.
+ * @param {string} [query.search] A search term to filter movies by title, genre, or director.
+ * @throws {ZodError} If the query parameters are invalid.
+ * @returns {Promise<TMoviePaginated | []>} Resloves to a validated Paginated collection of Movie objects or empty array
+ */
+export const getMoviesService = async (query: any): Promise<TMoviePaginated> => {
     const { page, limit, search } = getPaginationQuerySchema.parse(query);
 
     // Build the search query
@@ -20,27 +29,44 @@ export const getMoviesService = async (query: any) => {
         : {};
 
     // Use the pagination utility
-    const result = await paginate(Movie, {
+    const paginatedResult = await paginate<IMovie>(Movie, {
         page,
         limit,
         searchQuery
     });
 
+    const movieDTOs: TMovie[] = paginatedResult.data.map((movie) => ({
+        id: movie._id.toString(),
+        title: movie.title,
+        duration: movie.duration,
+        genre: movie.genre,
+        pgRating: movie.pgRating,
+        year: movie.year,
+        director: movie.director,
+        cast: movie.cast,
+        description: movie.description,
+        imgURL: movie.imgURL
+    }));
+
     // Validate the response data using Zod
     const validatedResult = moviePaginatedSchema.parse({
-        data: result.data,
-        totalPages: result.totalPages,
-        currentPage: result.currentPage
+        data: paginatedResult.data,
+        totalPages: paginatedResult.totalPages,
+        currentPage: paginatedResult.currentPage
     });
 
     return validatedResult;
 };
 
-export const updateMovieService = async (id: string, updates: MovieZod) => {
-    if (!id) {
-        throw new Error('Movie ID is required.');
-    }
-
+/**
+ * Updates a movie document in the database.
+ * Assumes the ID and update data have been validated by the controller.
+ * @param {string} id The ID of the movie to update.
+ * @param {Partial<TMovie>} updates The validated movie data to apply.
+ * @throws {Error} Throws an error if the movie is not found.
+ * @returns {Promise<TMovie>} A promise that resolves to the updated movie DTO
+ */
+export const updateMovieService = async (id: string, updates: Partial<TMovie>): Promise<TMovie> => {
     const updatedMovie = await Movie.findByIdAndUpdate(id, updates, {
         new: true,
         runValidators: true
@@ -50,7 +76,7 @@ export const updateMovieService = async (id: string, updates: MovieZod) => {
         throw new Error('Movie not found');
     }
 
-    const movieExportDto: MovieZod = {
+    const movieExportDto: TMovie = {
         id: updatedMovie._id.toString(),
         title: updatedMovie.title,
         duration: updatedMovie.duration,
@@ -63,14 +89,19 @@ export const updateMovieService = async (id: string, updates: MovieZod) => {
         imgURL: updatedMovie.imgURL
     };
 
-    return movieExportDto;
+    return movieSchema.parse(movieExportDto);
 };
 
-export const createMovieService = async (movieData: MovieZod) => {
-    const validatedMovieData = movieSchema.parse(movieData);
-
-    const newMovie = await Movie.create(validatedMovieData);
-    const movieExportDto: MovieZod = {
+/**
+ * Creates movie document, returns Movie DTO object
+ * Assumes the controller validated the movie data.
+ * @param {TMovie} movieData The Movie object.
+ * @throws {Error} Throws an error if the database opreation fails.
+ * @returns {Promise<TMovie>} A promise that resloves to the newly created movie DTO.
+ */
+export const createMovieService = async (movieData: TMovie): Promise<TMovie> => {
+    const newMovie = await Movie.create(movieData);
+    const movieExportDto: TMovie = {
         id: newMovie._id.toString(),
         title: newMovie.title,
         duration: newMovie.duration,
@@ -82,20 +113,47 @@ export const createMovieService = async (movieData: MovieZod) => {
         description: newMovie.description,
         imgURL: newMovie.imgURL
     };
-    return movieExportDto;
+    return movieSchema.parse(movieExportDto);
 };
 
-export const deleteMovieService = async (id: string) => {
-    if (!id) {
-        throw new Error('Movie ID is required');
+/**
+ * Deletes a movie and all its assosiated sessions within a transaction.
+ * Assumes the controller has validated the movie ID.
+ * @param {string} id The ID of the movie to delete.
+ * @throws {Error} Throws an error if the movie is not found or if the transaction fails.
+ * @returns {Promise<TMovie>} A promise that resolves to the DTO of the deleted movie.
+ */
+export const deleteMovieService = async (id: string): Promise<TMovie> => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        await Session.deleteMany({ movieId: id }, { session });
+        const deletedMovie = await Movie.findByIdAndDelete(id, { session });
+
+        if (!deletedMovie) {
+            await session.abortTransaction();
+            throw new Error('Movie not found.');
+        }
+
+        await session.commitTransaction();
+
+         return movieSchema.parse({
+            id: deletedMovie._id.toString(),
+            title: deletedMovie.title,
+            duration: deletedMovie.duration,
+            genre: deletedMovie.genre,
+            pgRating: deletedMovie.pgRating,
+            year: deletedMovie.year,
+            director: deletedMovie.director,
+            cast: deletedMovie.cast,
+            description: deletedMovie.description,
+            imgURL: deletedMovie.imgURL
+        });
+    } catch (error) {
+        await session.abortTransaction();
+        throw error
+    } finally {
+        session.endSession();
     }
-
-    await Session.deleteMany({ movieId: id });
-    const deletedMovie = await Movie.findByIdAndDelete(id);
-
-    if (!deletedMovie) {
-        throw new Error('Movie not found');
-    }
-
-    return deletedMovie;
 };
