21a0c9ef13c8e28f7f8d0b3ab99a2c1cc2748198
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-09-12 22:43:07 +0300

    refactor admin.session controller to use error handling middleware

--- API/src/controllers/admin.session.controller.ts
+++ API/src/controllers/admin.session.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { createSessionService, getSessionSeatLayoutService, getSessionsWithFiltersService } from '../services/sessionService';
 import { sessionFiltersSchema, createSessionSchema } from '../utils';
@@ -8,25 +8,13 @@ import { sessionFiltersSchema, createSessionSchema } from '../utils';
  * @desc Create a new movie session
  * @access Private (Admin)
  */
-export const createSession = async (req: Request, res: Response) => {
+export const createSession = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const validatedData = createSessionSchema.parse(req.body);
         const newSession = await createSessionService(validatedData);
         res.status(201).json({ session: newSession });
-    } catch (error: any) {
-        if (error.name === 'ZodError') {
-            return res.status(400).json({ error: 'Invalid session data provided.', details: error.message });
-        }
-
-        if (error.message.includes('already booked')) {
-            return res.status(409).json({ error: error.message }); // 409 Conflict
-        }
-
-        if (error.message.includes('not found')) {
-            return res.status(404).json({ error: error.message });
-        }
-
-        res.status(500).json({ error: error.message });
+    } catch (err: any) {
+        next(err)
     }
 };
 
@@ -35,17 +23,13 @@ export const createSession = async (req: Request, res: Response) => {
  * @desc Get sessions with optional filters for cinema, hall, movie, date, with pagination
  * @access Private (Admin)
  */
-export const getSessionsWithFilters = async (req: Request, res: Response) => {
+export const getSessionsWithFilters = async (req: Request, res: Response, next:NextFunction) => {
     try {
         const filters = sessionFiltersSchema.parse(req.query);
         const sessionsData = await getSessionsWithFiltersService(filters);
         res.status(200).json(sessionsData);
     } catch (err: any) {
-        if (err.name === 'ZodError') {
-            return res.status(400).json({ error: 'Invalid query parameters.', details: err.errors });
-        }
-
-        res.status(500).json({ error: err.message });
+       next(err)
     }
 };
 
