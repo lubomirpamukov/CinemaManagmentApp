347d623a991dbddc3a2ffd239c07c90067d97bc4
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-09-12 22:42:28 +0300

    refactor admin.cinema controller to use errorhandling middleware

--- API/src/controllers/admin.cinema.controller.ts
+++ API/src/controllers/admin.cinema.controller.ts
@@ -8,9 +8,7 @@ import {
 } from '../services/adminCinemaService';
 import mongoose from 'mongoose';
 import { cinemaSchema } from '../utils';
-import { type } from 'node:os';
 import { ZodError } from 'zod';
-import { error } from '../config/logging';
 import { CustomError } from '../middleware/errorHandler';
 
 /**
@@ -18,15 +16,12 @@ import { CustomError } from '../middleware/errorHandler';
  * @desc Get all cinemas
  * @access Private (Admin)
  */
-export const getCinemas = async (req: Request, res: Response) => {
+export const getCinemas = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const cinemas = await getCinemasService();
         res.status(200).json({ cinemas });
     } catch (err: any) {
-        if (err instanceof ZodError) {
-            return res.status(400).json({ error: 'Data validation failed', details: err.errors });
-        }
-        res.status(500).json({ error: err.message });
+        next(err)
     }
 };
 
@@ -57,28 +52,25 @@ export const getCinemaById = async (req: Request, res: Response, next: NextFunct
  * @desc Updates the data of a cinema object
  * @access Private (Admin)
  */
-export const updateCinema = async (req: Request, res: Response) => {
+export const updateCinema = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { id } = req.params;
 
         if (!mongoose.Types.ObjectId.isValid(id)) {
-            return res.status(400).json({ message: 'Invalid cinema ID format.' });
+            throw new CustomError('Invalid Cinema Id format.', 400)
         }
 
         const validUpdates = cinemaSchema.partial().parse(req.body);
 
         const updatedCinema = await updateCinemaByIdService(id, validUpdates);
 
-        res.status(200).json({ cinema: updatedCinema });
-    } catch (err: any) {
-        if (err instanceof ZodError) {
-            return res.status(400).json({ error: 'Updates validation failed', details: err.errors });
+        if (updatedCinema === null) {
+            throw new CustomError('Cinema not found.', 404)
         }
 
-        if (err.message.includes('not found')) {
-            return res.status(404).json({ error: err.message });
-        }
-        res.status(500).json({ error: err.message });
+        res.status(200).json({ cinema: updatedCinema });
+    } catch (err: any) {
+        next(err)
     }
 };
 
@@ -87,7 +79,7 @@ export const updateCinema = async (req: Request, res: Response) => {
  * @description Create a new cinema document
  * @access Private (Admin)
  */
-export const createCinema = async (req: Request, res: Response) => {
+export const createCinema = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const validCinema = cinemaSchema.parse(req.body);
 
@@ -95,13 +87,7 @@ export const createCinema = async (req: Request, res: Response) => {
 
         res.status(201).json(createdCinema);
     } catch (err: any) {
-        console.error('Create cinema error:', err);
-
-        if (err instanceof ZodError) {
-            return res.status(400).json({ error: 'Validation failed', details: err.errors });
-        }
-
-        res.status(500).json({ error: 'Failed to create cinema' });
+        next(err)
     }
 };
 
@@ -110,15 +96,18 @@ export const createCinema = async (req: Request, res: Response) => {
  * @description Deletec Cinema document.
  * @access Private (Admin)
  */
-export const deleteCinema = async (req: Request, res: Response) => {
+export const deleteCinema = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ error: 'Invalid cinema id' });
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            throw new CustomError('Invalid cinema Id format.', 400)
+        }
         const deletedCinema = await deleteCinemaService(id);
-        if (deletedCinema === null) return res.status(404).json({ error: 'Cinema not found' });
+        if (deletedCinema === null){
+            throw new CustomError('Cinema not found.', 404)
+        }
         res.status(204).end();
-    } catch (error: any) {
-        console.error('Delete cinema error', error);
-        res.status(500).json({ error: 'Failed to delete cinema' });
+    } catch (err: any) {
+        next(err)
     }
 };
