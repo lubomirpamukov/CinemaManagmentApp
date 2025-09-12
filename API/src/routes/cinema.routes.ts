8f5195eb462a1a278f75a67db5672ad4ecaa95cc
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-09-12 22:50:49 +0300

    test

--- API/src/routes/cinema.routes.ts
+++ API/src/routes/cinema.routes.ts
@@ -3,7 +3,7 @@ import { authentication, authorizeRoles } from '../middleware/auth.middleware';
 import { getCinemaCityByMovieId, getCinemasByCityAndMovie } from '../controllers/cinema.controller';
 
 const cinemaRouter: Router = express.Router();
-
+// test
 cinemaRouter.get('/movies/:movieId', authentication, authorizeRoles(['user']), getCinemaCityByMovieId);
 cinemaRouter.get('/by-city-and-movie', authentication, authorizeRoles(['user']),getCinemasByCityAndMovie);
 export default cinemaRouter;
